845e91d567bcfe8b12fcc8f24df5497c2a12414f
Echostt <31448222+Echostt@users.noreply.github.com>
2018-11-19 21:53:59 -0700

    Update tracking and turret vars

---
+++ UnwarrantedGame/Assets/Scripts/Single Use Scripts/LookAtTargetConstant.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTargetConstant : MonoBehaviour {
+
+    public GameObject target;
+
+	void Update () {
+        this.transform.LookAt(target.gameObject.transform.position, Vector3.up);
+    }
+}
--- UnwarrantedGame/Assets/Scripts/TurretController.cs
+++ UnwarrantedGame/Assets/Scripts/TurretController.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class TurretController : MonoBehaviour {
     public float fireRate;
     public float fireRange;
+    public float fireAngle;
 
     private float lastFireTime;
 
     void Update () {
         if (Time.time - lastFireTime > fireRate) {
             lastFireTime = Time.time;
-            Debug.DrawRay(this.transform.position, Vector3.forward * 5, Color.red, 1f);
-            Collider[] cols = Physics.OverlapSphere(this.gameObject.transform.position, fireRange, 1 << 10); //5f detect distance
+
+            Vector3 v = Vector3.forward;
+            v = Quaternion.Euler(
+                this.gameObject.transform.rotation.x,
+                this.gameObject.transform.rotation.y + fireAngle,
+                this.gameObject.transform.rotation.z) * v;
+            Vector3 vv = Vector3.forward;
+            vv = Quaternion.Euler(
+                this.gameObject.transform.rotation.x,
+                this.gameObject.transform.rotation.y + fireAngle * -1,
+                this.gameObject.transform.rotation.z) * vv;
+
+            Debug.DrawRay(this.transform.position, v * fireRange, Color.red, 1f);
+            Debug.DrawRay(this.transform.position, vv * fireRange, Color.red, 1f);
+
+            Debug.Log(vv);
+
+            Collider[] cols = Physics.OverlapSphere(this.gameObject.transform.position, fireRange, 1 << 10); //detect distance
             float dot;
             Vector3 characterToCollider;
             foreach (Collider col in cols) {
                 characterToCollider = (col.transform.position - transform.position).normalized;
                 dot = Vector3.Dot(characterToCollider, transform.forward);
-                if (dot >= Mathf.Cos(55)) {
+                if (dot >= Mathf.Cos(fireAngle)) {
                     //calculate firing angle
                     Vector3 dir = (col.gameObject.transform.position - this.gameObject.transform.position).normalized;
                     transform.LookAt(col.gameObject.transform);
